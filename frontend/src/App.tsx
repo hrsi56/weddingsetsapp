dc82081280f9356c72f50bae0d3542c90ec4a5bb
Yarden Viktor Dejorno <40130506+hrsi56@users.noreply.github.com>
2025-07-08 01:39:41 +0300

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -198,7 +198,7 @@ const NavBar: React.FC = () => {
               exit={{ opacity: 0, y: 20 }}
               transition={{ duration: 0.3 }}
             >
-              אישור הגעה
+              נבוא?
             </MotionButton>
           )}
         </AnimatePresence>
