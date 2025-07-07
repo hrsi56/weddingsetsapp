65b1f70749f794277f0b309f2369127cf70df6af
Yarden Viktor Dejorno <40130506+hrsi56@users.noreply.github.com>
2025-07-08 01:11:32 +0300

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -73,7 +73,7 @@ const NavBar: React.FC = () => {
 
   // START: לוגיקה לכפתור צף בגלילה
   const [showButton, setShowButton] = useState(false);
-  constֶ scrollTimeoutRef = useRef<ReturnType<typeof setTimeout> | null>(null);
+  const scrollTimeoutRef = useRef<ReturnType<typeof setTimeout> | null>(null);
 
 
   useEffect(() => {
