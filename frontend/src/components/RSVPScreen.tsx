91059c431587e23559281686c9f1c675043e6af0
Yarden Viktor Dejorno <hrsi56@gmail.com>
2025-07-28 13:47:18 +0300

    b

--- frontend/src/components/RSVPScreen.tsx
+++ frontend/src/components/RSVPScreen.tsx
@@ -151,7 +151,6 @@ const RSVPScreen: React.FC = () => {
           table.push({
             טלפון: g.phone,
             כיסא: "נא לגשת לכניסה לקבלת מקומות",
-            איזור: g.area ?? "-",
             אורחים: g.num_guests,
             שם: g.name,
           });
